44d3c463532664fc441e657ff92ee54c6add466c
LabRat3K <LabRat3K@users.noreply.github.com>
2023-03-05 16:07:28 -0500

    Tweak Pixel demo to add Keypad Matrix scanning

--- sample_pixel/pixel.c
+++ sample_pixel/pixel.c
@@ -41,9 +41,20 @@ APP_CONFIG(USB_BUS_POWERED, 250 MILLIAMPS);
  */
 
 // location of the LED, DATA output, and pushbutton
-//#define LED LATCbits.LATC2
+
 #define DATA LATAbits.LATA4
-#define SW1 PORTAbits.RA3
+#define SW1  PORTAbits.RA3
+
+// Defined as OUTPUTS
+#define ROW1 LATCbits.LATC4
+#define ROW2 LATCbits.LATC5
+#define ROW3 LATAbits.LATA5
+
+// Define as INPUTS
+#define COL1 PORTC.RC0
+#define COL2 PORTC.RC1
+#define COL3 PORTC.RC2
+#define COL4 PORTC.RC3
 
 #define uint8_t unsigned char
 
@@ -73,29 +84,56 @@ void inline send(char b)  {
  __endasm;
 }
 
-#define USETHIS
-#ifdef USETHIS
 
 // auxiliary functions to control the WS2812 NeoPixel LEDs
 void sendByte (unsigned char b);
-void sendRGB (unsigned char r, unsigned char g, unsigned char b);
 
 void sendArray (void) {
   uint8_t i;
+  __asm
+     bcf	INTCON,GIE
+  __endasm;
+
   for (i=0;i<sizeof(DisplayArray);i++) {
      sendByte(DisplayArray[i]);
   }
+  __asm
+     bsf	INTCON,GIE
+  __endasm;
 }
 
-void setArray(uint8_t index, uint8_t r,uint8_t g, uint8_t b) {
+void setPixel(uint8_t index, uint8_t r,uint8_t g, uint8_t b) {
    uint8_t offset = (index%12)*3;
 
-   DisplayArray[offset] = g;
-   DisplayArray[offset+1] =r;
-   DisplayArray[offset+2] =b;
+   if (index<21) {
+      DisplayArray[offset] = g;
+      DisplayArray[offset+1] =r;
+      DisplayArray[offset+2] =b;
+   }
 }
 
-void setBase(uint8_t r,uint8_t g, uint8_t b) {
+void setBase(signed char bid, uint8_t r, uint8_t g, uint8_t b) {
+   if (bid<0)
+     return;
+
+   if (bid>8)
+     return;
+
+   uint8_t offset = (12+bid)*3;  // Normallize then add to BASE offset
+
+   DisplayArray[offset] = g;
+   DisplayArray[offset+1] = r;
+   DisplayArray[offset+2] = b;
+
+}
+
+static void clearPixels() {
+  for (i=0;i<12*3;i++) {
+     DisplayArray[i] =0;
+  }
+}
+
+void floodBase(uint8_t r,uint8_t g, uint8_t b) {
    uint8_t i;
    for (i=36;i<22*3;i+=3) {
      DisplayArray[i] = g;
@@ -104,17 +142,46 @@ void setBase(uint8_t r,uint8_t g, uint8_t b) {
    }
 }
 
-// main function
-int app_main (void) {
-    
-    
-    // set up internal oscillator to run at 48 MHz
-    
+
+static void DrawLarsen(signed char index) {
+   setBase(index,   0, 0, 0x10);
+   setBase(index+1, 0, 0, 0x20);
+   setBase(index+2, 0, 0x10, 0x40);
+   setBase(index+3, 0, 0, 0x20);
+   setBase(index+4, 0, 0, 0x10);
+}
+
+signed char larsen_index;
+uint8_t larsen_count;
+signed char larsen_dir;  // Animation Direction
+
+void larsen_tick(void) {
+   larsen_count--;
+   if (larsen_count==0) {
+      larsen_index += larsen_dir;
+      larsen_count = 3;
+
+      floodBase(0,0,0);
+      DrawLarsen(larsen_index);
+
+      if (larsen_index>0x08) {
+         larsen_dir = -1;
+      } else if (larsen_index == -5) {
+         larsen_dir = 1;
+      } 
+      
+   } 
+}
+
+void app_init(void) {
     // LED is an output
-    TRISC = 0x00;
-    
+    //TRISC = 0x0F; //RC0-RC3 (Columns) are INPUTS
+    TRISC = 0x0; 
+    LATC = LATC&0xF0; 
+
     // turn off analog to digital converter
     ANSELA = 0;
+    ANSELC = 0;
     
     // button input
     TRISA = 0;
@@ -127,94 +194,181 @@ int app_main (void) {
     // Note: Get this wrong and all sorts of things don't work. ;o)
     T1CON = 0x35;
    
- 
+    larsen_index = -5;
+    larsen_dir = 1;
+    larsen_count = 3;
+}
+    
+uint8_t KEYS[3];
+
+const uint8_t ColorMap [12*3] = {
+           0x40,    0,      0 ,
+           0x40>>1, 0x40>>3, 0 ,
+           0x40>>2, 0x40>>2, 0,
+           0x40>>3, 0x40>>1, 0,
+           0     , 0x40   , 0,
+           0     , 0x40>>1, 0x40>>3,
+           0     , 0x40>>2, 0x40>>2,
+           0     , 0x40>>3, 0x40>>1,
+           0     , 0     , 0x40,
+           0x40>>3, 0     , 0x40>>1,
+           0x40>>2, 0     , 0x40>>2,
+           0x40>>1, 0     , 0x40>>3
+};
+
+
+static void checkRow0(void) {
+   if (KEYS[0]&0x01) {
+      setPixel(0,0x80,0,0);
+      larsen_dir = 1;
+    }
+   else 
+      setPixel(0,0,0,0);
+
+   if (KEYS[0]&0x02) 
+      setPixel(1,0,0x80,0);
+   else
+      setPixel(1,0,0,0);
+
+   if (KEYS[0]&0x04) 
+      setPixel(2,0,0,0x80);
+   else
+      setPixel(2,0,0,0);
+
+   if (KEYS[0]&0x08)
+      setPixel(3,0x80,0,0x80);
+   else
+      setPixel(3,0,0,0);
+}
+
+static void checkRow1(void) {
+   if (KEYS[1]&0x01) 
+      setPixel(4,0x80,0,0);
+   else 
+      setPixel(4,0,0,0);
+
+   if (KEYS[1]&0x02) 
+      setPixel(5,0,0x80,0);
+   else
+      setPixel(5,0,0,0);
+
+   if (KEYS[1]&0x04) 
+      setPixel(6,0,0,0x80);
+   else
+      setPixel(6,0,0,0);
+
+   if (KEYS[1]&0x08)
+      setPixel(7,0x80,0,0x80);
+   else
+      setPixel(7,0,0,0);
+} 
+
+static void checkRow2(void) {
+   if (KEYS[2]&0x01) 
+      setPixel(8,0x80,0,0);
+   else 
+      setPixel(8,0,0,0);
+
+   if (KEYS[2]&0x02) 
+      setPixel(9,0,0x80,0);
+   else
+      setPixel(9,0,0,0);
+
+   if (KEYS[2]&0x04) 
+      setPixel(10,0,0,0x80);
+   else
+      setPixel(10,0,0,0);
+
+   if (KEYS[2]&0x08)
+      setPixel(11,0x80,0,0x80);
+   else
+      setPixel(11,0,0,0);
+} 
+
+#define MAX_BRIGHTNESS 63 
+#define MIN_BRIGHTNESS 10
+
+#define BASE_EFFECT_SOLID  0
+#define BASE_EFFECT_LARSON 1
+#define BASE_EFFECT_NONE   2
+
+
+static void scanKeys() {
+   ROW1 = 1;
+   ROW2 = 0;
+   ROW3 = 0;
+   TRISC = 0x0F; //RC0-RC3 (Columns) are INPUTS
+   KEYS[0] = PORTC&0x0F;
+   TRISC = 0x00; //RC0-RC3 (Columns) are INPUTS
+   ROW1 = 0;
+   ROW2 = 1;
+   TRISC = 0x0F; //RC0-RC3 (Columns) are INPUTS
+   KEYS[1] = PORTC&0x0F;
+   TRISC = 0x00; //RC0-RC3 (Columns) are INPUTS
+   ROW2 = 0;
+   ROW3 = 1;
+   TRISC = 0x0F; //RC0-RC3 (Columns) are INPUTS
+   KEYS[2] = PORTC&0x0F;
+   TRISC = 0x00; //RC0-RC3 (Columns) are INPUTS
+   ROW3 = 0;
+}
+
+// main function
+int app_main (void) {
+    uint8_t base_effect = BASE_EFFECT_LARSON; 
     // declare variables used for animation
     unsigned char brt = 1; 
     signed char dir = 1;
-    int timebase = 0, stage = 0;
+    int stage = 0;
+
+    app_init();    
     
-    __asm
-      bcf	INTCON,GIE
-    __endasm;
-#define MAX_BRIGHTNESS 127 
-#define MIN_BRIGHTNESS 10
-    for (int j=0;j<12;j++) {
-        setArray(j,0,0,0);
-    }
-    setBase(90,0,90);
-    sendArray();
-
- // Color Wheel... 
-
-// R 0 -> 12*10
-//    Amber
-// G
-//    Teal
-// B
-//    Violet
-//
     // main loop
     while (1) {
-        // react to button press
+        switch(base_effect) {
+          case BASE_EFFECT_LARSON:
+	      larsen_tick();
+              break;
+          case BASE_EFFECT_SOLID:
+              floodBase(0x40,0x00,0x0);
+              break; 
+          default: // BASE_EFFECT_NONE
+              break;
+        }
+
+        scanKeys();
+	checkRow0();
+        checkRow1();
+        checkRow2();
+        // Brightness up/down 
         brt += dir;
         if (brt == MAX_BRIGHTNESS) {
            dir = -1;
         } else if (brt == MIN_BRIGHTNESS) {
            dir = 1;
 	   stage = (stage+1)%12;
-
-	   switch (stage) {
-             // R    RG    G    GB     B   BR 
-             // 0 1  2  3  4  5  6  7  8 9 10  11
-             case 0: setBase(48,0,  0); break;
-             case 1: setBase(0 ,24, 0); break;
-             case 2: setBase(48,48, 0); break;
-             case 3: setBase(24,48, 0); break;
-             case 4: setBase(0, 48, 0); break;
-
-             case 5: setBase(0, 48,24); break;
-             case 6: setBase(0, 48,48); break;
-             case 7: setBase(0, 24,48); break;
-             case 8: setBase(0,  0,48); break;
-             case 9: setBase(24, 0,48); break;
-             case 10: setBase(48,0,48); break;
-             case 11: setBase(48,0,24); break;
-
-           }
-
         }
-
-        // time the animation
-        /*timebase++;
-        if (timebase > 200) {
-            timebase = 0;
-            stage = (stage+1)&0x07;
-        }*/
-        
- 
-        // send out animation pattern
-        // R    RG    G    GB     B   BR 
-        // 0 1  2  3  4  5  6  7  8 9 10  11
-        setArray(stage,   brt,    0,      0 );
-        setArray(stage+1, brt>>1, brt>>3, 0 );
-        setArray(stage+2, brt>>2, brt>>2, 0 );
-        setArray(stage+3, brt>>3, brt>>1, 0 );
-        setArray(stage+4, 0     , brt   , 0 );
-        setArray(stage+5, 0     , brt>>1, brt>>3 );
-        setArray(stage+6, 0     , brt>>2, brt>>2 );
-        setArray(stage+7, 0     , brt>>3, brt>>1 );
-        setArray(stage+8, 0     , 0     , brt );
-        setArray(stage+9, brt>>3, 0     , brt>>1 );
-        setArray(stage+10,brt>>2, 0     , brt>>2 );
-        setArray(stage+11,brt>>1, 0     , brt>>3 );
-
-        //   __asm
-        //	 bsf	INTCON,GIE
-        //   __endasm;
-            
-        // Put some code to check for TMR roll over
-         // wait some time
-        //    __delay_ms(1);
+/*
+        if (KEYS[0]&0x01) {
+          setPixel(0,0,0x60,0);
+        } else {
+           // send out animation pattern
+           // R    RG    G    GB     B   BR 
+           // 0 1  2  3  4  5  6  7  8 9 10  11
+           setPixel(stage,   brt,    0,      0 );
+           setPixel(stage+1, brt>>1, brt>>3, 0 );
+           setPixel(stage+2, brt>>2, brt>>2, 0 );
+           setPixel(stage+3, brt>>3, brt>>1, 0 );
+           setPixel(stage+4, 0     , brt   , 0 );
+           setPixel(stage+5, 0     , brt>>1, brt>>3 );
+           setPixel(stage+6, 0     , brt>>2, brt>>2 );
+           setPixel(stage+7, 0     , brt>>3, brt>>1 );
+           setPixel(stage+8, 0     , 0     , brt );
+           setPixel(stage+9, brt>>3, 0     , brt>>1 );
+           setPixel(stage+10,brt>>2, 0     , brt>>2 );
+           setPixel(stage+11,brt>>1, 0     , brt>>3 );
+        }
+*/
 
         TMR1=64036; 
     
@@ -253,15 +407,5 @@ void sendByte (unsigned char b) {
     
 }
 
-// send red, green, and blue values in WS2812 protocol
-void sendRGB (unsigned char r, unsigned char g, unsigned char b) {
-
-    sendByte(g);
-    sendByte(r);
-    sendByte(b);
-    
-}
-
-#endif
 void app_interrupt( void ) {
 }
